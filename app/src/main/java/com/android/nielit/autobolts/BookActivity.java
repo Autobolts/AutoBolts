3c7c3269fec24d03fa3fa8d530fa9c869f153f6b
Nijil <nijilnirmal@gmail.com>
2016-11-29 01:45:45 +0530

    Reduced the size of images; Map Fragment working.

--- app/src/main/java/com/android/nielit/autobolts/BookActivity.java
+++ app/src/main/java/com/android/nielit/autobolts/BookActivity.java
@@ -1,36 +1,90 @@
 package com.android.nielit.autobolts;
 
+import android.*;
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.identity.intents.Address;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (googleServiceAvailable()) {
+            Toast.makeText(this, "Connected to Play Services", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_book);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.book_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.book_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.setDrawerListener(toggle);
+            toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+            initMap();
+        } else {
+            setContentView(R.layout.activity_home);
+        }
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    public boolean googleServiceAvailable() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int isAvailable = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (apiAvailability.isUserResolvableError(isAvailable)) {
+            Dialog dialog = apiAvailability.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't connect to Play Services", Toast.LENGTH_LONG).show();
+        }
+        return false;
     }
 
     @Override
@@ -55,7 +109,7 @@ public class BookActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
@@ -69,27 +123,27 @@ public class BookActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_profile) {
-            Intent intent = new Intent(getApplicationContext(),ProfileActivity.class);
+            Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_pchart) {
-            Intent intent = new Intent(getApplicationContext(),PeriodicActivity.class);
+            Intent intent = new Intent(getApplicationContext(), PeriodicActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_book) {
-            Intent intent = new Intent(getApplicationContext(),BookActivity.class);
+            Intent intent = new Intent(getApplicationContext(), BookActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_details) {
-            Intent intent = new Intent(getApplicationContext(),DetailsActivity.class);
+            Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_help) {
-            Intent intent = new Intent(getApplicationContext(),HelpActivity.class);
+            Intent intent = new Intent(getApplicationContext(), HelpActivity.class);
             startActivity(intent);
 
         } else if (id == R.id.nav_about) {
-            Intent intent = new Intent(getApplicationContext(),AboutActivity.class);
+            Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
             startActivity(intent);
         } else if (id == R.id.nav_signout) {
 
@@ -98,5 +152,82 @@ public class BookActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.book_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                List<String> permissionsNeeded = new ArrayList<>();
+
+                final List<String> permissionsList = new ArrayList<String>();
+                if (!addPermission(permissionsList, Manifest.permission.ACCESS_FINE_LOCATION))
+                    permissionsNeeded.add("Fine Location");
+                if (!addPermission(permissionsList, Manifest.permission.ACCESS_COARSE_LOCATION))
+                    permissionsNeeded.add("Course Location");
+
+                if (permissionsList.size() > 0) {
+                    if (permissionsNeeded.size() > 0) {
+                        // Need Rationale
+                        String message = "You need to grant access to " + permissionsNeeded.get(0);
+                        for (int i = 1; i < permissionsNeeded.size(); i++)
+                            message = message + " and " + permissionsNeeded.get(i);
+                        showMessageOKCancel(message,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        requestPermissions(permissionsList.toArray(new String[permissionsList.size()]), 124);
+                                    }
+                                });
+                        return;
+                    }
+                    requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+                            124);
+                    return;
+                }
+            }
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+
+            if (!shouldShowRequestPermissionRationale(permission))
+                return false;
+        }
+        return true;
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private void goToLocationZoom(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat,lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll,zoom);
+        mGoogleMap.moveCamera(update);
+    }
+
+    public void geoLocate(View view) throws IOException
+    {
+        EditText txtLoc = (EditText)findViewById(R.id.txtLoc);
+        String location = txtLoc.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<android.location.Address> list = gc.getFromLocationName(location,1);
+        android.location.Address address = list.get(0);
+
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+        goToLocationZoom(lat,lng,8.0f);
     }
 }
