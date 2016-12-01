811aa593128629812726c1639578e860b549afff
Nijil <nijilnirmal@gmail.com>
2016-12-01 23:31:40 +0530

    Successful Login and Reg

--- app/src/main/java/com/android/nielit/autobolts/LoginActivity.java
+++ app/src/main/java/com/android/nielit/autobolts/LoginActivity.java
@@ -4,14 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public static final String LOGIN_URL="http://10.0.2.2/dbvolly/login.php";
     Button btnLogin;
     Button btnPwd;
     Button btnSignUp;
+    public static final String KEY_EMAIL="email";
+    public static final String KEY_PASSWORD="password";
+    public EditText editTextEmail;
+    public EditText editTextPassword;
+    public String email;
+    public String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +37,64 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         btnLogin = (Button)findViewById(R.id.btnLogin);
         btnSignUp = (Button)findViewById(R.id.btnSingup);
-        btnLoginOnClick();
+        btnLogin.setOnClickListener(this);
         btnRegOnClick();
+        editTextEmail=(EditText)findViewById(R.id.txtUsername);
+        editTextPassword=(EditText)findViewById(R.id.txtRegPassword);
     }
 
-    public void btnLoginOnClick()
+    public void userLogin()
     {
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
-                startActivity(intent);
+
+                email=editTextEmail.getText().toString().trim();
+                password=editTextPassword.getText().toString().trim();
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,LOGIN_URL,new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        if(response.trim().equals("success"))
+                        {
+                            openProfile();
+                        }else
+                        {
+                            Toast.makeText(LoginActivity.this,response,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error)
+                            {
+                                Toast.makeText(LoginActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+
+                            }
+                        }){
+                @Override
+                protected Map<String,String> getParams() throws AuthFailureError {
+                    Map<String,String> map=new HashMap<String, String>();
+                    map.put(KEY_EMAIL,email);
+                    map.put(KEY_PASSWORD,password);
+                    return map;
+                }
+                };
+
+                RequestQueue requestQueue= Volley.newRequestQueue(this);
+                requestQueue.add(stringRequest);
+
             }
-        });
+
+        private void openProfile()
+    {
+        Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+        startActivity(intent);
     }
+    @Override
+    public void onClick(View v)
+    {
+        userLogin();
+    }
+
 
     public void btnRegOnClick()
     {
--- app/src/main/java/com/android/nielit/autobolts/RegisterActivity.java
+++ app/src/main/java/com/android/nielit/autobolts/RegisterActivity.java
@@ -6,8 +6,31 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String REGISTER_URL = "http://10.0.2.2/dbvolly/vollyRegister.php";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_FNAME = "fname";
+    public static final String KEY_LNAME = "lname";
+
+    private EditText editTextFname;
+    private EditText editTextLname;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
 
     Button btnReg;
 
@@ -17,18 +40,62 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        btnReg = (Button)findViewById(R.id.btnReg);
-        btnRegOnClick();
+        btnReg = (Button) findViewById(R.id.btnReg);
+        editTextEmail = (EditText) findViewById(R.id.txtRegEmail);
+        editTextPassword = (EditText) findViewById(R.id.txtRegPassword);
+        editTextFname = (EditText) findViewById(R.id.txtRegFirstName);
+        editTextLname = (EditText) findViewById(R.id.txtRegLastName);
+        btnReg.setOnClickListener(this);
     }
 
-    public void btnRegOnClick()
-    {
+    private void registerUser() {
+        final String fname = editTextFname.getText().toString().trim();
+        final String lname = editTextLname.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_LONG).show();
+            }
+        },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegisterActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams()  throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(KEY_FNAME, fname);
+                params.put(KEY_LNAME, lname);
+                params.put(KEY_EMAIL, email);
+                params.put(KEY_PASSWORD, password);
+                return params;
+            }
+
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void btnRegOnClick() {
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),UserDetails.class);
+                Intent intent = new Intent(getApplicationContext(), UserDetails.class);
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnReg) {
+            registerUser();
+        }
     }
 }
